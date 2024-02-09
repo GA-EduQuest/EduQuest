b83fe4af7bd2bdf512b8a34a6ef4716b148b8fac
Megan Coningsby <meganconingsby@gmail.com>
2024-02-10 10:43:38 +1100

    Simplifiy quest code and add quests.md for quest details

--- main_app/quests.py
+++ main_app/quests.py
@@ -1,8 +1,15 @@
 from .models import Profile, Subject, Assignment, Quest, ProfileAchievement, User
-from datetime import date, datetime
+from datetime import date
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-from django.db.models import F
+
+# Repeat Function to add quest to ProfileAchievement, add to XP and save user.profile
+
+def add_quest_xp_and_save(user, quest_name):
+    find_quest = Quest.objects.get(name=quest_name)
+    ProfileAchievement.objects.create(user=user, quest=find_quest)
+    user.profile.xp += ProfileAchievement.get_quest_xp(quest_name)
+    user.profile.save()
 
 # Functions that check & grant quests based on user actions. Note, some logic also sits on the models where relevant.
 
@@ -10,10 +17,7 @@ def grant_name_to_face_quest(user):
     if Quest.objects.exists():
         name_to_face_quest_name = 'A Name to a Face'
         if not ProfileAchievement.has_quest_achievement(user, name_to_face_quest_name):
-            name_to_face_quest = Quest.objects.get(name=name_to_face_quest_name)
-            ProfileAchievement.objects.create(user=user, quest=name_to_face_quest)
-            user.profile.xp += ProfileAchievement.get_quest_xp(name_to_face_quest_name)
-            user.profile.save()
+            add_quest_xp_and_save(user, name_to_face_quest_name)
 
 def grant_getting_started_quest(user, subject):
     if Quest.objects.exists():
@@ -21,20 +25,15 @@ def grant_getting_started_quest(user, subject):
         if not ProfileAchievement.has_quest_achievement(user, getting_started_quest_name):
             existing_subjects_count = Subject.objects.filter(user=user).exclude(pk=subject.pk).count()
             if existing_subjects_count == 0:
-                getting_started_quest = Quest.objects.get(name=getting_started_quest_name)
-                ProfileAchievement.objects.create(user=user, quest=getting_started_quest)
-                user.profile.xp += ProfileAchievement.get_quest_xp(getting_started_quest_name)
-                user.profile.save()
+                add_quest_xp_and_save(user, getting_started_quest_name)
 
 def grant_subject_explorer_quest(user):
     if Quest.objects.exists():
         subject_explorer_quest_name = 'Subject Explorer'
         subjects_count_by_field = Subject.objects.filter(user=user).values('field').distinct().count()
         if subjects_count_by_field >= 4 and not ProfileAchievement.has_quest_achievement(user, subject_explorer_quest_name):
-            subject_explorer_quest = Quest.objects.get(name=subject_explorer_quest_name)
-            ProfileAchievement.objects.create(user=user, quest=subject_explorer_quest)
-            user.profile.xp += ProfileAchievement.get_quest_xp(subject_explorer_quest_name)
-            user.profile.save()
+            add_quest_xp_and_save(user, subject_explorer_quest_name)
+
 
 def grant_exam_slayer_quest(user):
     if Quest.objects.exists():
@@ -42,20 +41,16 @@ def grant_exam_slayer_quest(user):
         if not ProfileAchievement.has_quest_achievement(user, exam_slayer_quest_name):
             subjects_with_passed_exams = Subject.objects.filter(user=user, exam_date__lt=date.today())
             if subjects_with_passed_exams.exists():
-                exam_slayer_quest = Quest.objects.get(name=exam_slayer_quest_name)
-                ProfileAchievement.objects.create(user=user, quest=exam_slayer_quest)
-                user.profile.xp += ProfileAchievement.get_quest_xp(exam_slayer_quest_name)
-                user.profile.save()
+                add_quest_xp_and_save(user, exam_slayer_quest_name)
+
 
 def grant_multitasking_maven_quest(user):
     if Quest.objects.exists():
         multitasking_maven_quest_name = 'Multitasking Maven'
         completed_subjects = Subject.objects.filter(user=user, progress=100, grade__in=['A', 'B']).count()
         if completed_subjects >= 2 and not ProfileAchievement.has_quest_achievement(user, multitasking_maven_quest_name):
-            multitasking_maven_quest = Quest.objects.get(name=multitasking_maven_quest_name)
-            ProfileAchievement.objects.create(user=user, quest=multitasking_maven_quest)
-            user.profile.xp += ProfileAchievement.get_quest_xp(multitasking_maven_quest_name)
-            user.profile.save()
+            add_quest_xp_and_save(user, multitasking_maven_quest_name)
+
 
 def grant_elite_leaderboard_quest():
     if Quest.objects.exists():
@@ -65,9 +60,8 @@ def grant_elite_leaderboard_quest():
         if top_profile:
             elite_champion_quest = Quest.objects.get(name=elite_champion_quest_name)
             if not ProfileAchievement.has_quest_achievement(top_profile.user, elite_champion_quest_name):
-                ProfileAchievement.objects.create(user=top_profile.user, quest=elite_champion_quest)
-                top_profile.user.profile.xp += ProfileAchievement.get_quest_xp(elite_champion_quest_name)
-                top_profile.user.profile.save()
+                add_quest_xp_and_save(top_profile.user, elite_champion_quest_name)
+
 
 def grant_master_the_basics_quest(user):
     if Quest.objects.exists():
@@ -77,42 +71,31 @@ def grant_master_the_basics_quest(user):
             if subject_count >= 4:
                 subjects_with_assignments = Subject.objects.filter(user=user, assignment__isnull=False).distinct()
                 if subjects_with_assignments.count() >= 4:
-                    master_the_basics_quest = Quest.objects.get(name=master_the_basics_quest_name)
-                    ProfileAchievement.objects.create(user=user, quest=master_the_basics_quest)
-                    user.profile.xp += ProfileAchievement.get_quest_xp(master_the_basics_quest_name)
-                    user.profile.save()
+                    add_quest_xp_and_save(user, master_the_basics_quest_name)
+
 
 def grant_time_management_pro_quest(user):
     if Quest.objects.exists():
         time_management_pro_quest_name = 'Time Management Pro'
         if not ProfileAchievement.has_quest_achievement(user, time_management_pro_quest_name):
             completed_users_assignments = Assignment.objects.filter(status='CM', subject__user=user)
-            print(completed_users_assignments)
             completed_early_assignments = sum(1 for assignment in completed_users_assignments if assignment.complete_date and assignment.complete_date <= assignment.due_date)
-            print(completed_early_assignments)
             if completed_early_assignments >= 2:
-                time_management_pro_quest = Quest.objects.get(name=time_management_pro_quest_name)
-                ProfileAchievement.objects.create(user=user, quest=time_management_pro_quest)
-                user.profile.xp += ProfileAchievement.get_quest_xp(time_management_pro_quest_name)
-                user.profile.save()
+                add_quest_xp_and_save(user, time_management_pro_quest_name)
+
 
 def grant_assignment_conqueror_quest(user):
     if Quest.objects.exists():
         quest_name = 'Assignment Conqueror'
         if not ProfileAchievement.has_quest_achievement(user, quest_name):
-            quest = Quest.objects.get(name=quest_name)
-            ProfileAchievement.objects.create(user=user, quest=quest)
-            user.profile.xp += ProfileAchievement.get_quest_xp(quest_name)
-            user.profile.save()
+            add_quest_xp_and_save(user, quest_name)
+
 
 def grant_grandmaster_quest(user):
     if Quest.objects.exists():
         grandmaster_quest_name = 'Grandmaster of EduQuest'
         if Quest.is_grandmaster(user) and not ProfileAchievement.has_quest_achievement(user, grandmaster_quest_name):
-            grandmaster_quest = Quest.objects.get(name=grandmaster_quest_name)
-            ProfileAchievement.objects.create(user=user, quest=grandmaster_quest)
-            user.profile.xp += ProfileAchievement.get_quest_xp(grandmaster_quest_name)
-            user.profile.save()
+            add_quest_xp_and_save(user, grandmaster_quest_name)
 
 
 
@@ -120,7 +103,6 @@ def grant_grandmaster_quest(user):
 @receiver(user_logged_in, sender=User)
 def check_achievements_on_login(sender, request, user, **kwargs):
     if Quest.objects.exists():
-        # Checks for some Quest achievements on login: Exam Slayer, Elite Leaderboard Champion, Grandmaster & Multitasking Maven
         grant_exam_slayer_quest(user)
         grant_grandmaster_quest(user)
         grant_multitasking_maven_quest(user)
---
+++ main_app/static/quests.md
@@ -0,0 +1,18 @@
+# EduQuest Quest Details
+
+Here you'll find all the details for each of the already added Quests to the EduQuest App. These need to be added to the Quest model for the Quest functionality to work throughout the app.
+
+You will need to add your own Badge Image URL to each quest, depending on where you are saving the image files.
+
+| Name                       | Description                                                                                                                                                                                 | XP   |
+| -------------------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ---- |
+| Getting Started            | Embark upon your epic quest journey. Choose your first subject to delve into the realms of knowledge.                                                                                       | 100  |
+| Master the Basics          | Forge your path as a sage of wisdom by summoning four subjects from the arcane depths of learning. Add and equip yourself with assignments to prove your mastery.                           | 300  |
+| Time Management Pro        | Harness the sands of time to your will. Demonstrate your prowess by completing at least two assignments before they arrive on these lands.                                                  | 500  |
+| Multitasking Maven         | Embrace the art of arcane multitasking. Skilfully balance the enchantments of two or more subjects, ensuring each maintains the shimmering glow of a minimum B grade.                       | 1000 |
+| Elite Leaderboard Champion | Ascend to the pinnacle of greatness. Scale the towering heights of the leaderboard, where only the most illustrious champions are crowned.                                                  | 1500 |
+| Grandmaster of EduQuest    | Prove yourself as the ultimate seeker of knowledge. Traverse the realms and conquer all quests that lay before you, becoming the legendary Grandmaster of EduQuest.                         | 5000 |
+| Exam Slayer                | Face the ultimate trial of wit and wisdom. Stand valiantly against the onslaught of the exam and emerge victorious, wielding your knowledge like a mighty sword.                            | 600  |
+| Assignment Conqueror       | Vanquish the challenges laid before you. Triumph over the trials of assignments and emerge victorious, proving your mastery of each task.                                                   | 500  |
+| Subject Explorer           | Embark on a journey of discovery across the vast lands of knowledge. Enrol in the mystical realms of at least one subject in four different fields, becoming a true explorer of the arcane. | 400  |
+| A Name to a Face           | Etch your mark upon the tapestry of history. Update your profile to ensure your presence is remembered, binding your name to your noble texts.                                              | 200  |
